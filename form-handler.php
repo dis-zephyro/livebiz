d2df4548205525fabf80b65fe2afe7f3c7d42cf8
dis-zephyro <zephyro@yandex.ru>
2015-10-19 14:41:57 +0300

    v1

--- form-handler.php
+++ form-handler.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mail = new PHPMailer();
     $mail->From = 'noreply@'. $_SERVER['SERVER_NAME']; // от кого
     $mail->FromName = $_SERVER['SERVER_NAME']; // от кого
-    $mail->AddAddress('livebz@yandex.ru', ''); // кому - адрес, Имя
+    $mail->AddAddress('livebz@yandex.ru, mail@mail.ru', ''); // кому - адрес, Имя
 
     $mail->IsHTML(true); // выставляем формат письма HTML
     $mail->Subject = $thm; // тема письма
