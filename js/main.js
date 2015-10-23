08d87df2eac86feb61ac6552914c1335ddae3672
dis-zephyro <zephyro@yandex.ru>
2015-10-23 21:56:32 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $('.header__nav li a').click(function(){
     var str=$(this).attr('href');
-    $.scrollTo(str, 500, {offset:-115});
+    $.scrollTo(str, 500, {offset: 0});
     return false;
 });
 
